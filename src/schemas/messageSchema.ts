749e58f2103e322c8fbea6c2941df745f3d43c7c
Yash Pathik <yashk71097@gmail.com>
2024-09-09 20:23:10 +0530

    First commit

---
+++ src/schemas/messageSchema.ts
@@ -0,0 +1,9 @@
+import { Content } from 'next/font/google';
+import { z } from 'zod';
+
+export const messageSchema = z.object({
+    content: z
+        .string()
+        .min(10, { message: 'content must be atleast 10 characters.' })
+        .max(300, { message: 'content must be less than 300 characters.' }),
+});
